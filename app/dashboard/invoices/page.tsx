339317686e86beeaab40b244966f79365304dfc6
gopika <gopika@mintsolutions.ae>
2026-06-08 11:41:40 +0400

    nextJS tutorial

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function InvoicesPage() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-4xl font-bold">Invoices</h1>
+    </div>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const secondaryFont = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+});
