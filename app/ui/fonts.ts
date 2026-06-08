be1bd3845d6063ff758929f86375f70c5387a9a0
gopika <gopika@mintsolutions.ae>
2026-06-08 12:08:20 +0400

    changing lusitana font secondaryFont

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,7 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, secondaryFont } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const secondaryFont = Lusitana({
+export const secondaryFont = secondaryFont({
     subsets: ['latin'],
     weight: ['400', '700'],
 });
